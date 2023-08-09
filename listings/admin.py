5539271660f1b0c5021567dc352edd1277d75e31
Dmytro Gul <guldmytro@gmail.com>
2023-08-09 17:30:34 +0300

    changed admin for listings

--- listings/admin.py
+++ listings/admin.py
@@ -56,7 +56,7 @@ class StreetAdmin(TranslatableAdmin):
 
 
 @admin.register(Listing)
-class ListingAdmin(LeafletGeoAdmin, TranslatableAdmin):
-    list_display = ('title', 'status', 'is_new_building', 'created', 'updated')
+class ListingAdmin(TranslatableAdmin):
+    list_display = ('title', 'status', 'created', 'updated')
     list_editable = ('status',)
-    list_filter = ('status', 'manager', 'is_new_building', 'created', 'updated',)
+    list_filter = ('status', 'manager', 'created', 'updated',)
