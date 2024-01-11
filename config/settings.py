355ea302a461dae8d29e0e4c9eccb83c506f0646
Dmytro Gul <guldmytro@gmail.com>
2024-01-11 16:14:34 +0200

    final fixes

--- config/settings.py
+++ config/settings.py
@@ -181,7 +181,7 @@ PARLER_DEFAULT_LANGUAGE_CODE = 'uk'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = Path('static/')
-STATIC_VERSION = '1.351'
+STATIC_VERSION = '1.352'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path('media/')
 
